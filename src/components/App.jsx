833754e4d7880cb2a10d318bfb7d326659d23472
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-11 02:58:57 +0300

    Change08

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,88 @@
-import React, {Component} from 'react'
-import Contacts from './Contacts/Contacts'
-import Filter from './Filter/Filter'
-import Form from './Form'
+import React, { Component } from 'react';
+import Contacts from './Contacts/Contacts';
+import Filter from './Filter/Filter';
+import Form from './Form';
 
 class App extends Component {
   state = {
-    contacts: [ 
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
-   }
+  };
 
-   onAddContact = newName => {
-    if (this.state.contacts.some(({ name }) => name === newName.name || name.toLowerCase() === newName.name.toLowerCase())) {
+  onAddContact = newName => {
+    if (
+      this.state.contacts.some(
+        ({ name }) =>
+          name === newName.name ||
+          name.toLowerCase() === newName.name.toLowerCase()
+      )
+    ) {
       alert(`${newName.name} is alredy in contacts`);
       return;
-    } this.setState(({contacts}) => ({contacts: [...contacts, newName]}))
     }
-  
-   onFilterContact = evt => {
-    this.setState({filter: evt.currentTarget.value})
-    }
+    this.setState(({ contacts }) => ({ contacts: [...contacts, newName] }));
+  };
 
-  getContactList = () => { 
+  onFilterContact = evt => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
+  getContactList = () => {
     const { filter, contacts } = this.state;
 
-    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
-  }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
 
   onRemoveContact = idContact => {
     this.setState(prevState => {
-    return ({
-    contacts: prevState.contacts.filter(contact => contact.id !== idContact)
-    })
-    })
-  }
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== idContact
+        ),
+      };
+    });
+  };
 
   render() {
     return (
       <>
-      <h1  style={{
-        marginTop: "25px",
-        textAlign: "center",
-        color: "#000000",
-      }}>Phonebook</h1>
-      <Form addToContact={this.onAddContact}/>
+        <h1
+          style={{
+            marginTop: '25px',
+            textAlign: 'center',
+            color: '#000000',
+          }}
+        >
+          Phonebook
+        </h1>
+        <Form addToContact={this.onAddContact} />
 
-      <h2 style={{
-        marginTop: "20px",
-        textAlign: "center",
-        color: "#000000",
-      }}>Contacts:</h2>
-      <Filter value={this.state.filter} filterContacts={this.onFilterContact}/>
-      <Contacts contacts={this.getContactList()} onRemove={this.onRemoveContact}/>
-
+        <h2
+          style={{
+            marginTop: '20px',
+            textAlign: 'center',
+            color: '#000000',
+          }}
+        >
+          Contacts:
+        </h2>
+        <Filter
+          value={this.state.filter}
+          filterContacts={this.onFilterContact}
+        />
+        <Contacts
+          contacts={this.getContactList()}
+          onRemove={this.onRemoveContact}
+        />
       </>
-    )
-  }  
-};
-export default App
+    );
+  }
+}
+export default App;
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,15 +1,21 @@
-import css from './Contacts.module.css'
+import css from './Contacts.module.css';
 
-const Contacts = ({contacts, onRemove}) => {
-return (
+const Contacts = ({ contacts, onRemove }) => {
+  return (
     <ul className={css.contactList}>
-        {contacts.map(({id, name, number})=>(
-            <li key={name} className={css.contactListItem}>
-                {name}: {number} <button type="button" onClick={() => onRemove(id)} className={css.btnContactList}>Delete</button>
-            </li>
-        ))}
+      {contacts.map(({ id, name, number }) => (
+        <li key={name} className={css.contactListItem}>
+          {name}: {number}{' '}
+          <button
+            type="button"
+            onClick={() => onRemove(id)}
+            className={css.btnContactList}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
     </ul>
-)
-}
-export default Contacts
-
+  );
+};
+export default Contacts;
